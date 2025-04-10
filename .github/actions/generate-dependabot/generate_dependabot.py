bc397eef269a5c6fd1f1a98f184829a91658aa4c
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-04-10 13:52:50 +0200

    ci(dependabot): fix

--- .github/actions/generate-dependabot/generate_dependabot.py
+++ .github/actions/generate-dependabot/generate_dependabot.py
@@ -122,10 +122,11 @@ updates:
     for ecosystem, dirs in ecosystem_dirs.items():
         # TODO: skip this for now, figure out later.
         # This should be its own entry, separate from production dependencies.
-        if "tools" in dirs:
+        if dirs == ["tools"]:
             continue
 
         # Format directories as YAML list
+        dirs.remove("tools")  # do not mix dev tooling into production dependencies
         dir_entries = '["' + '", "'.join(dirs) + '"]'
         config += f"""
   - package-ecosystem: "{ecosystem}"
